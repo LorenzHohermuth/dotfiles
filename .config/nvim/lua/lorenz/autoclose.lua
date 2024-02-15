06316d7a655aac505a966028cd244fb4535905cc
lorenzhohermuth <lofloho@gmail.com>
2024-02-15 13:17:35 +0100

    moved nvim folder out

--- .config/nvim/lua/lorenz/autoclose.lua
+++
@@ -1,7 +0,0 @@
-require("autoclose").setup({
-   keys = {
-     ["("] = { escape = false, close = true, pair = "()", disabled_filetypes = {} },
-     ["{"] = { escape = false, close = true, pair = "{}", disabled_filetypes = {} },
-     ["<"] = { escape = false, close = true, pair = "<>", disabled_filetypes = {} },
-   },
-})
