9520019beffdb21253f6cd187e3a3b3484f3c11a
lorenzhohermuth <lofloho@gmail.com>
2024-04-04 08:36:12 +0200

    Screenmenu changes

--- .zshrc
+++ .zshrc
@@ -101,7 +101,10 @@ source $ZSH/oh-my-zsh.sh
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 #
 
-export PATH=$PATH:"$HOME/go/bin:$HOME/.local/bin:$HOME/scripts"
+export PATH=$PATH:"$GOPATH/bin:$HOME/scripts"
+export GOPATH=$HOME/go
+
+export XDG_CONFIG_HOME="$HOME/.config"
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
---
+++ cht.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+languages=`echo "golang lua java bash" | tr ' ' '\n'`
+core_utils=`echo "docker docker-compose find mv sed awk" | tr ' ' '\n'`
+
+selected=`printf "$languages\n$core_utils" | fzf`
+read -p "query: " query
+
+if printf $languages | grep -qs $selected; then
+	tmux neww bash -c "curl cht.sh/$selected/`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1;done"
+else
+	tmux neww bash -c "curl cht.sh/$selected~$query & while [ : ]; do sleep 1;done"
+	fi
